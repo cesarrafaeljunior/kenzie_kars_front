4301062506841855cf071c39ed9fd7db35e757b2
César Rafael <cesarrdev@gmail.com>
2023-04-12 16:23:32 -0300

    feat: Created Component Footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,1 +1,30 @@
-export const Footer = () => {};
+import { Flex, Img, Text, IconButton } from "@chakra-ui/react";
+import { ChevronUpIcon } from "@chakra-ui/icons";
+
+export const Footer = () => {
+  return (
+    <Flex
+      as="section"
+      bg={"grey.0"}
+      minHeight={"140px"}
+      padding={"0 59px"}
+      alignItems={"center"}
+      justifyContent={"space-between"}
+      fontFamily={"Inter, sans-serif"}
+      fontSize={"sm"}
+    >
+      <Img src="/imgs/motor_shop.png" alt="logo" objectFit={"contain"} />
+      <Text color={"grey.whiteFixed"}>
+        @2022 - Todos os direitos reservados
+      </Text>
+      <IconButton
+        bg="grey.1"
+        aria-label="Footer"
+        icon={<ChevronUpIcon />}
+        _focus={{ bg: "grey.1" }}
+        _hover={{ bg: "grey.2" }}
+        size={"lg"}
+      />
+    </Flex>
+  );
+};
