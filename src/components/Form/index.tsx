de380d4d4d8a7f109e23ed209088c50bf8ca856b
César Rafael <cesarrdev@gmail.com>
2023-04-12 00:24:21 -0300

    feat: Created component Form

---
+++ src/components/Form/index.tsx
@@ -0,0 +1,7 @@
+const Login = () => {};
+const Register = () => {};
+const EditAddress = () => {};
+const CreateAd = () => {};
+const EditAd = () => {};
+
+export const Form = { Login, Register, EditAddress, CreateAd, EditAd };
