f05f9c2be014a239c009c63d08f152e62ef4a0c5
matheuscg13 <matheusdecastrogoncalves@gmail.com>
2023-04-12 12:37:11 -0300

    feat: product card component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,3 @@
-import { Box } from "@chakra-ui/react";
-import Head from "next/head";
-
-
 export default function Home() {
-    return (
-        <>
-            <Head>
-                <title>Create Next App</title>
-            </Head>
-           <main>  <Box> Projeto fica aqui </Box></main>
-           
-        </>
-    );
+  return <></>;
 }
