17abcf1a9f708b19d342d29ffb3df979edb04ea6
César Rafael <cesarrdev@gmail.com>
2023-04-12 00:05:49 -0300

    feat: Created Modal base Component

---
+++ src/components/Modal/index.tsx
@@ -0,0 +1,23 @@
+export const ModalVehicleImage = () => {};
+
+export const ModalSuccessAccunt = () => {};
+
+export const ModalEditAddress = () => {};
+
+export const ModalCreateAd = () => {};
+
+export const ModalEditAd = () => {};
+
+export const ModalSuccessAd = () => {};
+
+export const ModalRemoveAdConfirmation = () => {};
+
+export const Modal = {
+  ModalVehicleImage,
+  ModalSuccessAccunt,
+  ModalEditAddress,
+  ModalCreateAd,
+  ModalEditAd,
+  ModalSuccessAd,
+  ModalRemoveAdConfirmation,
+};
