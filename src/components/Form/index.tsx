1e5cae1fb05810b6d85f1680354115e3b298fc57
César Rafael <cesarrdev@gmail.com>
2023-04-12 09:49:10 -0300

    fix: Removed components

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,7 +1,5 @@
-const Login = () => {};
-const Register = () => {};
 const EditAddress = () => {};
 const CreateAd = () => {};
 const EditAd = () => {};
 
-export const Form = { Login, Register, EditAddress, CreateAd, EditAd };
+export const Form = { EditAddress, CreateAd, EditAd };
--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,11 +1,1 @@
-const Default = () => {};
-const Authenticated = () => {};
-const AuthenticatedDefault = () => {};
-const AuthenticatedAnnouncement = () => {};
-
-export const Navbar = {
-  Default,
-  Authenticated,
-  AuthenticatedDefault,
-  AuthenticatedAnnouncement,
-};
+export const Navbar = () => {};
