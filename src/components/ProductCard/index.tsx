df902f8c44c1a4887add3b9f157c890b06e996cb
Thomas Schreiner <iteano.thom@hotmail.com>
2023-04-13 09:32:37 -0300

    fix: fix indentation of files

--- src/components/ProductCard/index.tsx
+++ src/components/ProductCard/index.tsx
@@ -1,78 +1,61 @@
 import { ListItem, Box, Image, Heading, Text } from "@chakra-ui/react";
 
 export const ProductCard = (advertData: any) => {
-  console.log(advertData);
-  return (
-    <ListItem maxWidth={"320px"}>
-      <Box backgroundColor={"grey.7"}>
-        <Image src={advertData.advertData.coverImage}></Image>
-      </Box>
-      <Heading
-        color={"grey.1"}
-        marginTop={"15px"}
-        fontSize={"16px"}
-        fontWeight={"semibold"}
-      >
-        {advertData.advertData.model}
-      </Heading>
-      <Text
-        marginTop={"15px"}
-        color={"grey.2"}
-        fontSize={"14px"}
-        fontWeight={"normal"}
-        textOverflow={"ellipsis"}
-        paddingRight={"10px"}
-      >
-        {advertData.advertData.description}
-      </Text>
-      <Box
-        gap={"8px"}
-        marginTop={"20px"}
-        alignItems={"center"}
-        display={"flex"}
-      >
-        <Image
-          src={advertData.advertData.adverter.profileImage}
-          height={"32px"}
-          width={"32px"}
-          borderRadius={"50%"}
-        ></Image>
-        <Text fontWeight={"medium"}>
-          {advertData.advertData.adverter.adverterName}
-        </Text>
-      </Box>
-      <Box marginTop={"15px"} display={"flex"} justifyContent={"space-between"}>
-        <Box display={"flex"} gap={"8px"}>
-          <Text
-            fontWeight={"medium"}
-            color={"brand.1"}
-            fontSize={"14px"}
-            backgroundColor={"brand.4"}
-            padding={"4px"}
-            borderRadius={"4px"}
-          >
-            {advertData.advertData.mileage}
-          </Text>
-          <Text
-            fontWeight={"medium"}
-            fontSize={"14px"}
-            color={"brand.1"}
-            backgroundColor={"brand.4"}
-            padding={"4px"}
-            borderRadius={"4px"}
-          >
-            {advertData.advertData.year}
-          </Text>
-        </Box>
-        <Text
-          fontWeight={"bold"}
-          fontSize={"16px"}
-          color={"grey.1"}
-          textDecoration={""}
-        >
-          {advertData.advertData.price}
-        </Text>
-      </Box>
-    </ListItem>
-  );
+    console.log(advertData);
+    return (
+        <ListItem maxWidth={"320px"}>
+            <Box backgroundColor={"grey.7"}>
+                <Image src={advertData.advertData.coverImage}></Image>
+            </Box>
+            <Heading color={"grey.1"} marginTop={"15px"} fontSize={"16px"} fontWeight={"semibold"}>
+                {advertData.advertData.model}
+            </Heading>
+            <Text
+                marginTop={"15px"}
+                color={"grey.2"}
+                fontSize={"14px"}
+                fontWeight={"normal"}
+                textOverflow={"ellipsis"}
+                paddingRight={"10px"}
+            >
+                {advertData.advertData.description}
+            </Text>
+            <Box gap={"8px"} marginTop={"20px"} alignItems={"center"} display={"flex"}>
+                <Image
+                    src={advertData.advertData.adverter.profileImage}
+                    height={"32px"}
+                    width={"32px"}
+                    borderRadius={"50%"}
+                ></Image>
+                <Text fontWeight={"medium"}>{advertData.advertData.adverter.adverterName}</Text>
+            </Box>
+            <Box marginTop={"15px"} display={"flex"} justifyContent={"space-between"}>
+                <Box display={"flex"} gap={"8px"}>
+                    <Text
+                        fontWeight={"medium"}
+                        color={"brand.1"}
+                        fontSize={"14px"}
+                        backgroundColor={"brand.4"}
+                        padding={"4px"}
+                        borderRadius={"4px"}
+                    >
+                        {advertData.advertData.mileage}
+                    </Text>
+                    <Text
+                        fontWeight={"medium"}
+                        fontSize={"14px"}
+                        color={"brand.1"}
+                        backgroundColor={"brand.4"}
+                        padding={"4px"}
+                        borderRadius={"4px"}
+                    >
+                        {advertData.advertData.year}
+                    </Text>
+                </Box>
+                <Text fontWeight={"bold"} fontSize={"16px"} color={"grey.1"} textDecoration={""}>
+                    {advertData.advertData.price}
+                </Text>
+            </Box>
+        </ListItem>
+    );
 };
--- src/styles/components/Button.ts
+++ src/styles/components/Button.ts
@@ -1,111 +1,111 @@
 import { defineStyleConfig, ComponentStyleConfig } from "@chakra-ui/react";
 
 export const ButtonStyle: ComponentStyleConfig = {
-  baseStyle: {
-    fontWeight: "semibold",
-    borderRadius: "4px",
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "center",
-    alignContent: "center",
-    fontFamily: "Inter, sans-serif",
-  },
-  sizes: {
-    sm: {
-      fontSize: "14px",
-      h: "38px",
-      px: "20px",
-      py: "12px",
+    baseStyle: {
+        fontWeight: "semibold",
+        borderRadius: "4px",
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "center",
+        alignContent: "center",
+        fontFamily: "Inter, sans-serif",
     },
-    md: {
-      fontSize: "16px",
-      h: "48px",
-      px: "28px",
-      py: "12px",
+    sizes: {
+        sm: {
+            fontSize: "14px",
+            h: "38px",
+            px: "20px",
+            py: "12px",
+        },
+        md: {
+            fontSize: "16px",
+            h: "48px",
+            px: "28px",
+            py: "12px",
+        },
     },
-  },
-  variants: {
-    grey1: {
-      borderColor: "#0B0D0D",
-      bg: "#0B0D0D",
-      color: "#ffffff",
-      _hover: { bg: "#212529", borderColor: "#212529" },
+    variants: {
+        grey1: {
+            borderColor: "#0B0D0D",
+            bg: "#0B0D0D",
+            color: "#ffffff",
+            _hover: { bg: "#212529", borderColor: "#212529" },
+        },
+        negative: {
+            borderColor: "#DEE2E6",
+            bg: "#DEE2E6",
+            color: "#495057",
+            _hover: { bg: "#CED4DA", borderColor: "#CED4DA" },
+        },
+        disable: {
+            borderColor: "#CED4DA",
+            bg: "#CED4DA",
+            color: "#FFFFFF",
+        },
+        brand1: {
+            borderColor: "#4529E6",
+            bg: "#4529E6",
+            color: "#FFFFFF",
+            _hover: { bg: "#5126EA", borderColor: "#5126EA" },
+        },
+        brandOpacity: {
+            borderColor: "#EDEAFD",
+            bg: "#EDEAFD",
+            color: "#4529E6",
+            _hover: { bg: "#B0A6F0", borderColor: "#B0A6F0" },
+        },
+        light: {
+            borderColor: "#FDFDFD",
+            bg: "#FDFDFD",
+            color: "#212529",
+        },
+        outlineLight: {
+            borderColor: "#FDFDFD",
+            bg: "#FDFDFD",
+            color: "#FDFDFD",
+            _hover: { color: "#212529" },
+        },
+        outline: {
+            borderColor: "#0B0D0D",
+            bg: "#FDFDFD",
+            color: "#0B0D0D",
+            _hover: { bg: "#212529", borderColor: "#212529", color: "#FDFDFD" },
+        },
+        outline2: {
+            borderColor: "#ADB5BD",
+            bg: "#FDFDFD",
+            color: "#0B0D0D",
+            _hover: { bg: "#212529", borderColor: "#212529", color: "#FDFDFD" },
+            _focus: { bg: "#4529E6", borderColor: "#4529E6", color: "#ffff" },
+        },
+        outlineBrand: {
+            borderColor: "#4529E6",
+            bg: "#FDFDFD",
+            color: "#4529E6",
+            _hover: { bg: "#EDEAFD", borderColor: "#4529E6" },
+        },
+        alert: {
+            borderColor: "#FFE5E5",
+            bg: "#FFE5E5",
+            color: "#CD2B31",
+            _hover: { bg: "#FDD8D8", borderColor: "#FDD8D8" },
+        },
+        sucess: {
+            borderColor: "#DDF3E4",
+            bg: "#DDF3E4",
+            color: "#18794E",
+            _hover: { bg: "#CCEBD7", borderColor: "#CCEBD7" },
+        },
+        brandDisable: {
+            borderColor: "#B0A6F0",
+            bg: "#B0A6F0",
+            color: "#ffffff",
+        },
     },
-    negative: {
-      borderColor: "#DEE2E6",
-      bg: "#DEE2E6",
-      color: "#495057",
-      _hover: { bg: "#CED4DA", borderColor: "#CED4DA" },
+    defaultProps: {
+        size: "md",
+        variant: "brand1",
     },
-    disable: {
-      borderColor: "#CED4DA",
-      bg: "#CED4DA",
-      color: "#FFFFFF",
-    },
-    brand1: {
-      borderColor: "#4529E6",
-      bg: "#4529E6",
-      color: "#FFFFFF",
-      _hover: { bg: "#5126EA", borderColor: "#5126EA" },
-    },
-    brandOpacity: {
-      borderColor: "#EDEAFD",
-      bg: "#EDEAFD",
-      color: "#4529E6",
-      _hover: { bg: "#B0A6F0", borderColor: "#B0A6F0" },
-    },
-    light: {
-      borderColor: "#FDFDFD",
-      bg: "#FDFDFD",
-      color: "#212529",
-    },
-    outlineLight: {
-      borderColor: "#FDFDFD",
-      bg: "#FDFDFD",
-      color: "#FDFDFD",
-      _hover: { color: "#212529" },
-    },
-    outline: {
-      borderColor: "#0B0D0D",
-      bg: "#FDFDFD",
-      color: "#0B0D0D",
-      _hover: { bg: "#212529", borderColor: "#212529", color: "#FDFDFD" },
-    },
-    outline2: {
-      borderColor: "#ADB5BD",
-      bg: "#FDFDFD",
-      color: "#0B0D0D",
-      _hover: { bg: "#212529", borderColor: "#212529", color: "#FDFDFD" },
-      _focus: { bg: "#4529E6", borderColor: "#4529E6", color: "#ffff" },
-    },
-    outlineBrand: {
-      borderColor: "#4529E6",
-      bg: "#FDFDFD",
-      color: "#4529E6",
-      _hover: { bg: "#EDEAFD", borderColor: "#4529E6" },
-    },
-    alert: {
-      borderColor: "#FFE5E5",
-      bg: "#FFE5E5",
-      color: "#CD2B31",
-      _hover: { bg: "#FDD8D8", borderColor: "#FDD8D8" },
-    },
-    sucess: {
-      borderColor: "#DDF3E4",
-      bg: "#DDF3E4",
-      color: "#18794E",
-      _hover: { bg: "#CCEBD7", borderColor: "#CCEBD7" },
-    },
-    brandDisable: {
-      borderColor: "#B0A6F0",
-      bg: "#B0A6F0",
-      color: "#ffffff",
-    },
-  },
-  defaultProps: {
-    size: "md",
-    variant: "brand1",
-  },
 };
 
 export const Button = defineStyleConfig(ButtonStyle);
