f5300252f911e9f0ddaface731794efaa383ae00
Thomas Schreiner <iteano.thom@hotmail.com>
2023-04-17 20:11:51 -0300

    feat: implement mock of user in the context

---
+++ src/interfaces/user.interfaces.ts
@@ -0,0 +1,12 @@
+export interface iUser {
+  id: string;
+  name: string;
+  email: string;
+  cpf: string;
+  phone_number: string;
+  birthdate: string;
+  description: string;
+  is_seller: boolean;
+  created_at: string;
+  updated_at: string;
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import { AdvertProvider } from "@/contexts/advert.context";
+import { UserProvider } from "@/contexts/user.context";
 import { theme } from "@/styles/globals";
 import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
@@ -6,9 +7,11 @@ import type { AppProps } from "next/app";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
-      <AdvertProvider>
-        <Component {...pageProps} />
-      </AdvertProvider>
+      <UserProvider>
+        <AdvertProvider>
+          <Component {...pageProps} />
+        </AdvertProvider>
+      </UserProvider>
     </ChakraProvider>
   );
 }
