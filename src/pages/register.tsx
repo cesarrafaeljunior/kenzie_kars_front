53b033465a4aad5bea327f2b820ad335cc2ab404
César Rafael <cesarrdev@gmail.com>
2023-04-20 12:48:34 -0300

    feat: Added MocalCotnainer component in the page register

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,14 +1,26 @@
-import { Box, Center } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Center,
+  Text,
+  useDisclosure,
+  useToast,
+} from "@chakra-ui/react";
 import { Form } from "@/components/Form";
 import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
+import { ModalContainer } from "@/components/Modal";
 
 const registerPage = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   return (
     <>
       <Header />
       <Center backgroundColor={"grey.8"}>
-        <Form.CreateProfile />
+        <Button onClick={onOpen}>Abrir modal</Button>
+        <ModalContainer.ModalSuccessAccount isOpen={isOpen} onClose={onClose} />
+        <Form.CreateProfile onOpen={onOpen} />
       </Center>
       <Footer />
     </>
