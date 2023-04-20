c6be2b6d948b86bb085a84c3ac2d86ebfb39979d
César Rafael <cesarrdev@gmail.com>
2023-04-20 12:49:48 -0300

    fix: Protocol changed from https to http

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 export const apiSearchCEP = async (cep: string) =>
-  await axios.get(`https://viacep.com.br/ws/${cep}/json/`);
+  await axios.get(`http://viacep.com.br/ws/${cep}/json/`);
 
 export const api = axios.create({
-  baseURL: "https://localhost:3001",
+  baseURL: "http://localhost:3001",
   timeout: 5000,
 });
