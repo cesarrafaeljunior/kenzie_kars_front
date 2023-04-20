7c0e51763abd68d3d112926fb148a99e35c5acf9
matheuscg13 <matheusdecastrogoncalves@gmail.com>
2023-04-20 09:54:07 -0300

    fix: fix home page mock and advert interface

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,100 +3,101 @@ import { Header } from "@/components/Header";
 import { ModalContainer } from "@/components/Modal";
 import { PaginationNumbers } from "@/components/PaginationNumbers";
 import { ProductCard } from "@/components/ProductCard";
+import { iAdvert } from "@/interfaces/advert.interfaces";
 import { Box, Button, Center, Input } from "@chakra-ui/react";
 import { Image, Heading, Text, List } from "@chakra-ui/react";
 
 export default function Home() {
-  const advertsData = [
+  const advertsData: iAdvert[] = [
     {
-      coverImage:
-        "https://o.remove.bg/downloads/5efb80fa-a972-4d3e-ba88-78702fc7950b/5723fab70e21634575011f03qr-635-gb-01-eps-removebg-preview.png",
-      model: "Marea Weekend Turbo",
+      id: "6e6a0f02-b1f0-4361-8679-9c4941eee212",
+      title: "It's a Car",
+      brand: "Chevrolet",
+      model: "Clio",
+      fuel: "Gasolina",
+      color: "Vermelha",
+      year: 2013,
+      mileage: 0,
+      price: 80000,
       description:
-        "lorem ipusn domdaddbasd dbasojdbn bdias bdiasdb klncim odijad oijas",
-      adverter: {
-        profileImage:
-          "https://elements-video-cover-images-0.imgix.net/files/245057943/preview.jpg?auto=compress%2Cformat&fit=min&h=394&w=700&s=da883a13a8656e741737f2a2780dcf21",
-        adverterName: "Seu Luiz",
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+      cover_image: "/imgs/EXTERIOR-frontSidePilotNear-1653845164710.png",
+      location: "97010530",
+      is_avaliable: true,
+      created_at: "2023-04-19T18:41:45.487Z",
+      updated_at: "2023-04-19T18:41:45.487Z",
+      user: {
+        id: "af8a1c69-424c-4769-8bec-01bcae520e1b",
+        name: "Thomas",
+        email: "thom@mail.com",
+        cpf: "12345678910",
+        phone_number: "54981215552",
+        birthdate: "1999-11-27T02:00:00.000Z",
+        description: "Digite aqui uma descrição.",
+        is_seller: true,
+        created_at: "2023-04-11T18:54:16.819Z",
+        updated_at: "2023-04-11T20:11:58.604Z",
       },
-      mileage: "100000km",
-      year: "2005",
-      price: "R$ 15.000,00",
     },
     {
-      coverImage:
-        "https://o.remove.bg/downloads/5efb80fa-a972-4d3e-ba88-78702fc7950b/5723fab70e21634575011f03qr-635-gb-01-eps-removebg-preview.png",
-      model: "Marea Weekend Turbo",
+      id: "85bd7380-f4f3-459b-a17f-dac41bfb6913",
+      title: "It's a Car",
+      brand: "Chevrolet",
+      model: "Clio",
+      fuel: "Gasolina",
+      color: "Vermelha",
+      year: 2013,
+      mileage: 0,
+      price: 80000,
       description:
-        "lorem ipusn domdaddbasd dbasojdbn bdias bdiasdb klncim odijad oijas",
-      adverter: {
-        profileImage:
-          "https://elements-video-cover-images-0.imgix.net/files/245057943/preview.jpg?auto=compress%2Cformat&fit=min&h=394&w=700&s=da883a13a8656e741737f2a2780dcf21",
-        adverterName: "Seu Luiz",
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+      cover_image: "/imgs/EXTERIOR-frontSidePilotNear-1653845164710.png",
+      location: "97010530",
+      is_avaliable: true,
+      created_at: "2023-04-19T20:33:02.755Z",
+      updated_at: "2023-04-19T20:33:02.755Z",
+      user: {
+        id: "af8a1c69-424c-4769-8bec-01bcae520e1b",
+        name: "Thomas",
+        email: "thom@mail.com",
+        cpf: "12345678910",
+        phone_number: "54981215552",
+        birthdate: "1999-11-27T02:00:00.000Z",
+        description: "Digite aqui uma descrição.",
+        is_seller: true,
+        created_at: "2023-04-11T18:54:16.819Z",
+        updated_at: "2023-04-11T20:11:58.604Z",
       },
-      mileage: "100000km",
-      year: "2005",
-      price: "R$ 15.000,00",
     },
     {
-      coverImage:
-        "https://o.remove.bg/downloads/5efb80fa-a972-4d3e-ba88-78702fc7950b/5723fab70e21634575011f03qr-635-gb-01-eps-removebg-preview.png",
-      model: "Marea Weekend Turbo",
+      id: "ecb992a6-eb3a-4c80-a2b5-79863739b68e",
+      title: "It's a Car",
+      brand: "Chevrolet",
+      model: "Clio",
+      fuel: "Gasolina",
+      color: "Vermelha",
+      year: 2013,
+      mileage: 0,
+      price: 80000,
       description:
-        "lorem ipusn domdaddbasd dbasojdbn bdias bdiasdb klncim odijad oijas",
-      adverter: {
-        profileImage:
-          "https://elements-video-cover-images-0.imgix.net/files/245057943/preview.jpg?auto=compress%2Cformat&fit=min&h=394&w=700&s=da883a13a8656e741737f2a2780dcf21",
-        adverterName: "Seu Luiz",
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+      cover_image: "/imgs/EXTERIOR-frontSidePilotNear-1653845164710.png",
+      location: "97010530",
+      is_avaliable: true,
+      created_at: "2023-04-19T21:04:48.684Z",
+      updated_at: "2023-04-19T21:04:48.684Z",
+      user: {
+        id: "af8a1c69-424c-4769-8bec-01bcae520e1b",
+        name: "Thomas",
+        email: "thom@mail.com",
+        cpf: "12345678910",
+        phone_number: "54981215552",
+        birthdate: "1999-11-27T02:00:00.000Z",
+        description: "Digite aqui uma descrição.",
+        is_seller: true,
+        created_at: "2023-04-11T18:54:16.819Z",
+        updated_at: "2023-04-11T20:11:58.604Z",
       },
-      mileage: "100000km",
-      year: "2005",
-      price: "R$ 15.000,00",
-    },
-    {
-      coverImage:
-        "https://o.remove.bg/downloads/5efb80fa-a972-4d3e-ba88-78702fc7950b/5723fab70e21634575011f03qr-635-gb-01-eps-removebg-preview.png",
-      model: "Marea Weekend Turbo",
-      description:
-        "lorem ipusn domdaddbasd dbasojdbn bdias bdiasdb klncim odijad oijas",
-      adverter: {
-        profileImage:
-          "https://elements-video-cover-images-0.imgix.net/files/245057943/preview.jpg?auto=compress%2Cformat&fit=min&h=394&w=700&s=da883a13a8656e741737f2a2780dcf21",
-        adverterName: "Seu Luiz",
-      },
-      mileage: "100000km",
-      year: "2005",
-      price: "R$ 15.000,00",
-    },
-    {
-      coverImage:
-        "https://o.remove.bg/downloads/5efb80fa-a972-4d3e-ba88-78702fc7950b/5723fab70e21634575011f03qr-635-gb-01-eps-removebg-preview.png",
-      model: "Marea Weekend Turbo",
-      description:
-        "lorem ipusn domdaddbasd dbasojdbn bdias bdiasdb klncim odijad oijas",
-      adverter: {
-        profileImage:
-          "https://elements-video-cover-images-0.imgix.net/files/245057943/preview.jpg?auto=compress%2Cformat&fit=min&h=394&w=700&s=da883a13a8656e741737f2a2780dcf21",
-        adverterName: "Seu Luiz",
-      },
-      mileage: "100000km",
-      year: "2005",
-      price: "R$ 15.000,00",
-    },
-    {
-      coverImage:
-        "https://o.remove.bg/downloads/5efb80fa-a972-4d3e-ba88-78702fc7950b/5723fab70e21634575011f03qr-635-gb-01-eps-removebg-preview.png",
-      model: "Marea Weekend Turbo",
-      description:
-        "lorem ipusn domdaddbasd dbasojdbn bdias bdiasdb klncim odijad oijas",
-      adverter: {
-        profileImage:
-          "https://elements-video-cover-images-0.imgix.net/files/245057943/preview.jpg?auto=compress%2Cformat&fit=min&h=394&w=700&s=da883a13a8656e741737f2a2780dcf21",
-        adverterName: "Seu Luiz",
-      },
-      mileage: "100000km",
-      year: "2005",
-      price: "R$ 15.000,00",
     },
   ];
 
