27105ac8dfe498f8bf79923b90b6a8973613c6e3
César Rafael <cesarrdev@gmail.com>
2023-04-22 04:25:01 -0300

    feat: Added new properties in input typing

--- src/interfaces/components.interfaces.ts
+++ src/interfaces/components.interfaces.ts
@@ -14,6 +14,7 @@ export interface iInput {
   placeholder: string;
   register: UseFormRegisterReturn;
   borderColor?: string;
+  value?: string;
 }
 
 export interface iInputPassword extends iInput {
