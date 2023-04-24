8ebe401eb75c44a40cc1278b074058d39df8f1f4
matheuscg13 <matheusdecastrogoncalves@gmail.com>
2023-04-24 10:43:14 -0300

    feat: add dinamic home page

--- src/contexts/advert.context.tsx
+++ src/contexts/advert.context.tsx
@@ -1,13 +1,26 @@
+import { iAdvert } from "@/interfaces/advert.interfaces";
 import { iAdvertContext, iContextProps } from "@/interfaces/context.interfaces";
-import { createContext, useContext, useState } from "react";
+import { api } from "@/services/api";
+import { createContext, useContext, useEffect, useState } from "react";
 
 const AdvertContext = createContext<iAdvertContext>({} as iAdvertContext);
 
 export const AdvertProvider = ({ children }: iContextProps) => {
   const [modalVehicleImage, setModalVehicleImage] = useState<string>("");
+  const [advertsList, setAdvertsList] = useState<iAdvert[]>([]);
 
+  useEffect(() => {
+    async function loadAdverts() {
+      const { data } = await api.get("/advertised");
+      console.log(data);
+      setAdvertsList(data);
+    }
+    loadAdverts();
+  }, []);
   return (
-    <AdvertContext.Provider value={{ modalVehicleImage, setModalVehicleImage }}>
+    <AdvertContext.Provider
+      value={{ modalVehicleImage, setModalVehicleImage, advertsList }}
+    >
       {children}
     </AdvertContext.Provider>
   );
--- src/interfaces/context.interfaces.ts
+++ src/interfaces/context.interfaces.ts
@@ -1,5 +1,6 @@
 import { Dispatch, ReactNode, SetStateAction } from "react";
 import { iUser } from "./user.interfaces";
+import { iAdvert } from "./advert.interfaces";
 
 export interface iContextProps {
   children: ReactNode;
@@ -8,6 +9,7 @@ export interface iContextProps {
 export interface iAdvertContext {
   modalVehicleImage: string;
   setModalVehicleImage: Dispatch<SetStateAction<string>>;
+  advertsList: iAdvert[]
 }
 
 export interface iUserContext {
