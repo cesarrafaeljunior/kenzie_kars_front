221fafeae86dea9cff9102e52c70240005d82ce7
Thomas Schreiner <iteano.thom@hotmail.com>
2023-04-25 18:02:26 -0300

    feat: create profile page for user and seller

--- src/interfaces/pages.interfaces.ts
+++ src/interfaces/pages.interfaces.ts
@@ -1,5 +1,9 @@
+import { iAdvertListByUser } from "./advert.interfaces";
 import { iUser } from "./user.interfaces";
 
 export interface iProfileProps {
   user: iUser;
+}
+export interface iSellerProfileProps {
+  seller: iAdvertListByUser;
 }
