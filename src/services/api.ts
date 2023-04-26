9235c9d11314fec3f465614c33221ea1ddafc660
Thomas Schreiner <iteano.thom@hotmail.com>
2023-04-25 22:56:07 -0300

    fix: fix localhost port

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,7 +4,7 @@ export const apiSearchCEP = async (cep: string) =>
   await axios.get(`https://viacep.com.br/ws/${cep}/json/`);
 
 export const api = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "http://localhost:3001",
   timeout: 5000,
 });
 
