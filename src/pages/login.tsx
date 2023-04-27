8254fa7d89eea42071dec0efdcf07a508280f3bc
César Rafael <cesarrdev@gmail.com>
2023-04-27 13:39:16 -0300

    fix: Added modal of resetPassword in Login Page

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,14 +1,18 @@
-import { Box, Center } from "@chakra-ui/react";
+import { Box, Button, Center, useDisclosure } from "@chakra-ui/react";
 import { Form } from "@/components/Form";
 import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
+import { ModalContainer } from "@/components/Modal";
 
 const loginPage = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
   return (
     <>
       <Header />
+
+      <ModalContainer.ModalRecoverPassword isOpen={isOpen} onClose={onClose} />
       <Center minH={"calc(100vh - 80px - 140px)"} backgroundColor={"grey.8"}>
-        <Form.Login />
+        <Form.Login onOpen={onOpen} />
       </Center>
       <Footer />
     </>
