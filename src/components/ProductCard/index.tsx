b07c09f7cc03fb7603aa5ae20ba4fb94074b74da
Thomas Schreiner <iteano.thom@hotmail.com>
2023-05-01 14:44:25 -0300

    feat: adjust card size and add functionality to redirect to advertise page

--- src/components/ProductCard/index.tsx
+++ src/components/ProductCard/index.tsx
@@ -10,11 +10,14 @@ import {
   Center,
   Button,
   Flex,
+  Spacer,
+  Badge,
 } from "@chakra-ui/react";
+import Link from "next/link";
 import { useRouter } from "next/router";
 
 interface iProductCard {
-  advertData: Omit<iAdvert, "user">;
+  advertData: Omit<iAdvert, "user"> | iAdvert;
   seller?: iUser | null;
   isSeller?: boolean;
 }
@@ -27,61 +30,79 @@ export const ProductCard = ({
   const router = useRouter();
 
   return (
-    <ListItem maxWidth={"312px"} minWidth={"312px"} p={"16px"}>
-      <Box backgroundColor={"grey.7"} w="max-content">
-        <Image src={advertData.cover_image} width={{ base: "300px" }} />
-      </Box>
-      <Heading
-        color={"grey.1"}
-        marginTop={"15px"}
-        fontSize={"16px"}
-        fontWeight={"semibold"}
-      >
-        {advertData.model}
-      </Heading>
-      <Text
-        marginTop={"15px"}
-        color={"grey.2"}
-        fontSize={"14px"}
-        fontWeight={"normal"}
-        textOverflow={"ellipsis"}
-        paddingRight={"10px"}
-      >
-        {advertData.description}
-      </Text>
-      {seller && (
+    <ListItem maxWidth={"312px"} minWidth={"312px"}>
+      <Link href={`/home/${advertData.id}`}>
         <Box
-          gap={"8px"}
-          marginTop={"20px"}
-          alignItems={"center"}
-          display={"flex"}
+          backgroundColor={"grey.7"}
+          h={"168px"}
+          px={"16px"}
+          py={"8px"}
+          position={"relative"}
         >
-          <Center
-            as={"p"}
-            bg={"brand.2"}
-            minW={"32px"}
-            boxSize={"32px"}
-            borderRadius={"50%"}
-            fontWeight={"bold"}
-            fontSize={"14px"}
-            color={"white"}
-          >
-            {`${seller.name[0]}${
-              seller.name.split(" ").length > 1
-                ? seller.name.split(" ", 2)[1][0]
-                : ""
-            }`}
-          </Center>
-          <Text fontWeight={"medium"}>{seller.name}</Text>
+          {router.pathname == "/profile/[id]" ? (
+            <Badge
+              position={"absolute"}
+              top={"11px"}
+              left={"16px"}
+              variant={advertData.is_avaliable ? "brand1" : "disabled"}
+            >
+              {advertData.is_avaliable ? "Ativo" : "Inativo"}
+            </Badge>
+          ) : null}
+          <Image
+            src={advertData.cover_image}
+            width={{ base: "300px" }}
+            maxH={"100%"}
+            objectFit={"contain"}
+            borderRadius={"2px"}
+          />
         </Box>
-      )}
-      <Box
-        marginTop={"15px"}
-        display={"flex"}
-        flexDirection="column"
-        justifyContent={"space-between"}
-      >
-        <Box display={"flex"} gap={"8px"}>
+        <Heading
+          color={"grey.1"}
+          marginTop={"15px"}
+          fontSize={"16px"}
+          fontWeight={"semibold"}
+        >
+          {`${advertData.brand} - ${advertData.model}`}
+        </Heading>
+        <Text
+          marginTop={"15px"}
+          color={"grey.2"}
+          fontSize={"14px"}
+          fontWeight={"normal"}
+          textOverflow={"ellipsis"}
+          paddingRight={"10px"}
+        >
+          {advertData.description}
+        </Text>
+        {seller && (
+          <Box
+            gap={"8px"}
+            marginTop={"20px"}
+            alignItems={"center"}
+            display={"flex"}
+          >
+            <Center
+              as={"p"}
+              bg={"brand.2"}
+              minW={"32px"}
+              boxSize={"32px"}
+              borderRadius={"50%"}
+              fontWeight={"bold"}
+              fontSize={"14px"}
+              color={"white"}
+            >
+              {`${seller.name[0]}${
+                seller.name.split(" ").length > 1
+                  ? seller.name.split(" ", 2)[1][0]
+                  : ""
+              }`}
+            </Center>
+            <Text fontWeight={"medium"}>{seller.name}</Text>
+          </Box>
+        )}
+
+        <Box display={"flex"} gap={"8px"} marginTop={"15px"}>
           <Text
             fontWeight={"medium"}
             color={"brand.1"}
@@ -102,26 +123,27 @@ export const ProductCard = ({
           >
             {advertData.year}
           </Text>
+          <Spacer />
+          <Text
+            fontWeight={"bold"}
+            fontSize={"16px"}
+            color={"grey.1"}
+            textDecoration={""}
+          >
+            {formatValues(advertData.price, "BRL")}
+          </Text>
         </Box>
-        <Text
-          fontWeight={"bold"}
-          fontSize={"16px"}
-          color={"grey.1"}
-          textDecoration={""}
-        >
-          {formatValues(advertData.price, "BRL")}
-        </Text>
-        {router.pathname == "/profile" && isSeller ? (
-          <Flex gap={"16px"} m="16px 0 0 0">
-            <Button h="38px" variant={"outline"}>
-              Editar
-            </Button>
-            <Button h="38px" variant={"outline"}>
-              Ver detalhes
-            </Button>
-          </Flex>
-        ) : null}
-      </Box>
+      </Link>
+      {router.pathname == "/profile" && isSeller ? (
+        <Flex gap={"16px"} m="16px 0 0 0">
+          <Button h="38px" variant={"outline"}>
+            Editar
+          </Button>
+          <Button h="38px" variant={"outline"}>
+            Ver detalhes
+          </Button>
+        </Flex>
+      ) : null}
     </ListItem>
   );
 };
